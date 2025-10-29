b1114383f73d604f5e48b8786dd9ad998c5f42fc
Juan Carlos Mendoza Castillo <106772857+JCCJN04@users.noreply.github.com>
2025-10-29 15:31:10 -0600

    Export AuthError from utils package

--- microservicios/alert_service/utils/__init__.py
+++ microservicios/alert_service/utils/__init__.py
@@ -1,5 +1,13 @@
 """Utility helpers for the alert service."""
 
-from .auth import token_required  # noqa: F401
+from .auth import AuthError, token_required  # noqa: F401
 from .helpers import send_audit_event, send_analytics_event  # noqa: F401
 from .responses import auto_response  # noqa: F401
+
+__all__ = [
+    "AuthError",
+    "token_required",
+    "send_audit_event",
+    "send_analytics_event",
+    "auto_response",
+]
